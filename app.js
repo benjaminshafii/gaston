f59f1cae72b25802602b76cbe640bedd851e179d
Benjamin Shafii <ben@prologe.io>
2023-08-23 16:46:17 -0700

    second checkpoint

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const maxHeightInput = document.getElementById("max-height");
 const startMeasurementButton = document.getElementById("start-measurement");
 const estimatedHeightOutput = document.getElementById("estimated-height");
 const accelerationOutput = document.getElementById("acceleration"); // New DOM element
+const fallingCountOutput = document.getElementById("falling-count");
 
 // Defining the accelerometer data
 let accelerometerData = { x: 0, y: 0, z: 0 };
@@ -15,6 +16,19 @@ let estimatedHeight = 0;
 
 // Defining the gravity constant
 const gravity = 9.81;
+
+// Defining the previous time
+let previousTime = null;
+
+// Defining the velocity
+let velocity = 0;
+
+// Defining the height
+let height = 0;
+
+// Defining the falling flag
+let isFalling = false;
+
 if (window.DeviceMotionEvent) {
   console.log("hello");
   window.addEventListener("devicemotion", handleAccelerometer, true);
@@ -22,36 +36,57 @@ if (window.DeviceMotionEvent) {
   alert("Your device does not support the Device Motion API.");
 }
 
+let fallingCount = 0;
+
 // Function to handle the accelerometer data
 function handleAccelerometer(event) {
+  const currentTime = new Date().getTime();
+  const deltaTime = previousTime ? (currentTime - previousTime) / 1000 : 0;
+  previousTime = currentTime;
+
   accelerometerData = {
     x: event.accelerationIncludingGravity.x,
     y: event.accelerationIncludingGravity.y,
     z: event.accelerationIncludingGravity.z,
   };
 
-  console.log(event, accelerometerData);
-  // Update the acceleration in the DOM
-  accelerationOutput.textContent = `X: ${accelerometerData.x.toFixed(
-    2
-  )}, Y: ${accelerometerData.y.toFixed(2)}, Z: ${accelerometerData.z.toFixed(
-    2
-  )}`;
-}
-
-// Function to calculate the estimated height
-function calculateEstimatedHeight() {
   const totalAcceleration = Math.sqrt(
     accelerometerData.x * accelerometerData.x +
       accelerometerData.y * accelerometerData.y +
       accelerometerData.z * accelerometerData.z
   );
 
-  const fallTime = Math.sqrt((2 * totalAcceleration) / gravity);
-  estimatedHeight = 0.5 * gravity * fallTime * fallTime;
+  // Check if the object is falling
+  if (totalAcceleration < gravity) {
+    isFalling = true;
+    velocity += totalAcceleration * deltaTime;
+    height += velocity * deltaTime;
 
-  // Update the estimated height in the DOM
-  estimatedHeightOutput.textContent = (estimatedHeight / maxHeight).toFixed(2);
+    if (!isFalling && height > 0.2) {
+      // If the object was not falling before and the height is more than 20cm
+      fallingCount++; // Increment the falling count
+      fallingCountOutput.textContent = `Falling count: ${fallingCount}`; // Update the UI
+    }
+  } else {
+    if (isFalling && height > 0.2) {
+      // If the object was falling and the height is more than 20cm
+      fallingCount++; // Increment the falling count
+      fallingCountOutput.textContent = `Falling count: ${fallingCount}`; // Update the UI
+    }
+    isFalling = false;
+    velocity = 0;
+    height = 0;
+  }
+
+  // Update the acceleration and height in the DOM
+  accelerationOutput.textContent = `X: ${accelerometerData.x.toFixed(
+    2
+  )}, Y: ${accelerometerData.y.toFixed(2)}, Z: ${accelerometerData.z.toFixed(
+    2
+  )}`;
+  estimatedHeightOutput.textContent = isFalling
+    ? `Falling from height: ${height.toFixed(2)} m`
+    : "Not falling";
 }
 
 // Event listener for the start measurement button
@@ -70,10 +105,6 @@ startMeasurementButton.addEventListener("click", () => {
     alert("Your device does not support the Device Motion API.");
     return;
   }
-
-  // Start the measurement
-  setInterval(calculateEstimatedHeight, 100);
-  calculateEstimatedHeight();
 });
 
 function requestMotionPermission() {
